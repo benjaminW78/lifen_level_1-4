8d7d89528be29bb9e837884552ac616fd9b1d515
benjamin winckell <benjamin@foxintelligence.fr>
2019-02-21 00:49:47 +0100

    refactor: modify main.js to be more reliable and cleaner

--- level1/nodeJS/main.js
+++ level1/nodeJS/main.js
@@ -1,4 +1,3 @@
-const Jsonfile = require('jsonfile')
 const writeJsonFile = require('write-json-file')
 
 const { workers: Workers, shifts: Shifts } = require('../data')
@@ -11,20 +10,30 @@ async function main() {
             return acc
         }, {})
 
+        const payload = []
+
         Shifts.forEach((item) => {
             const userId = item.user_id
             if (!workerMap[userId]) {
                 console.log(`warning worker with user_id ${userId} don't exist `)
                 return
             }
-            if (!workerMap[userId].price) {
-                workerMap[userId].price = 0
+
+            if (!payload[userId]) {
+                payload[userId] = { id: userId }
             }
-            workerMap[userId].price += workerMap[userId].price_per_shift
-            return workerMap[userId]
+
+            if (!payload[userId].price) {
+                payload[userId].price = 0
+            }
+            payload[userId].price += workerMap[userId].price_per_shift
+
         })
 
-        await writeJsonFile(outputPath, Workers.map(item => ({ id: item.id, price: item.price })))
+        await writeJsonFile(outputPath, {
+            workers: payload.filter(item => item !== null)
+        })
+
         console.log('output.json successfully created')
     } catch (err) {
         console.log(err)

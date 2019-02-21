b7aa680959f4c6469ff31fc6a21042b35717fe4a
benjamin winckell <benjamin@foxintelligence.fr>
2019-02-21 14:29:01 +0100

    feat: level3 schedules based on workers status and shift rate by day

---
+++ level3/nodeJS/const_day_price_multiplicator.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+    1: { name: 'monday', multi: 1 },
+    2: { name: 'tuesday', multi: 1 },
+    3: { name: 'wednesday', multi: 1 },
+    4: { name: 'thursday', multi: 1 },
+    5: { name: 'friday', multi: 1 },
+    6: { name: 'saturday', multi: 2 },
+    7: { name: 'sunday', multi: 2 }
+})
---
+++ level3/nodeJS/main.js
@@ -0,0 +1,50 @@
+const WriteJsonFile = require('write-json-file')
+const Moment = require('moment')
+
+const WorkersStatus = require('./const_workers_status')
+const DaysMultiplicators = require('./const_day_price_multiplicator')
+const { workers: Workers, shifts: Shifts } = require('../data')
+
+async function main() {
+    try {
+        const outputPath = './dir/output.json'
+        const workerMap = Workers.reduce((acc, item) => {
+            acc[item.id] = item
+            acc[item.id].price_per_shift = WorkersStatus[acc[item.id].status]
+            return acc
+        }, {})
+
+        const payload = []
+
+        Shifts.forEach((item) => {
+            const userId = item.user_id
+            const weekDay = Moment(item.start_date, 'YYYY-MM-DD').isoWeekday()
+
+            if (!workerMap[userId]) {
+                console.log(`warning worker with user_id ${userId} don't exist `)
+                return
+            }
+
+            if (!payload[userId]) {
+                payload[userId] = { id: userId }
+            }
+
+            if (!payload[userId].price) {
+                payload[userId].price = 0
+            }
+
+            payload[userId].price += workerMap[userId].price_per_shift * DaysMultiplicators[weekDay].multi
+
+        })
+
+        await WriteJsonFile(outputPath, {
+            workers: payload.filter(item => item !== null)
+        })
+
+        console.log('output.json successfully created')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
